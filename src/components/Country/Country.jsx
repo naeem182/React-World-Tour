fab64ff14caa914eff4792a94160d0d0f7afae62
naeem <naeemislamjr182@gmail.com>
2023-09-10 19:44:46 +0600

    daynamically take data from feth show flag of each country

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -2,10 +2,11 @@ import "./Country.css"
 
 const Country = ({ country }) => {
 
-    const { name } = country;
+    const { name, flags } = country;
     return (
         <div className="country">
             <h3>country: {name?.common}</h3>
+            <img src={flags.png} alt="" />
         </div>
     )
 }
