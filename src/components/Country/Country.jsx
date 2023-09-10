8951ea1ccf4fbfa409c1ea08046fd3b0f14106b6
naeem <naeemislamjr182@gmail.com>
2023-09-10 22:54:40 +0600

    button er khela

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import "./Country.css"
 
 const Country = ({ country }) => {
 
     const { name, flags, population, area } = country;
+
+    const [visited, setvisited] = useState(false);
+    //
+    const handlevisit = () => {
+        setvisited(!visited)
+    }
+
+
+
     return (
         <div className="country">
             <div className="">
@@ -12,9 +22,15 @@ const Country = ({ country }) => {
                 }} src={flags.png} alt="" />
                 <p>poulation:{population}</p>
                 <p>Area:{area}</p>
+                {/* <button style={{
+                    background: "purple"
+                }} onClick={handlevisit}>visit</button> */}
+                {/* //dynaic button */}
                 <button style={{
                     background: "purple"
-                }}>visit</button>
+                }} onClick={handlevisit}>{visited ? "visited" : "going"}</button>
+                {/* button click to show something  */}
+                {visited && "i have visited this country"}
             </div>
         </div>
     )
